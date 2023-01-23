e0121d4bee286a6aaf7ffa8b9d0b50e1aaf56ae8
Francis Champagne <accounts@fcis.io>
2023-01-23 16:52:05 -0500

    chore: improver counter challenge

--- src/pages/counter.tsx
+++ src/pages/counter.tsx
@@ -1,23 +1,32 @@
 import { css } from "@emotion/react";
 
 const CounterPage = () => {
+  const handleCount = () => {};
+
   return (
     <main>
       <h1>Counter</h1>
       <h2>Create a counter that has a min. 0 and max. 3</h2>
       <blockquote>
-        <p>You want to minimize the rerenders</p>
+        <p>
+          1. There should also be a counter on the amount of clicks. It&apos;s
+          value will be logged `console.log()` when either of the buttons are
+          clicked.
+          <br />
+          {'For example, clicking on "+", "-", "+" should log: 3'}
+        </p>
+        <p>2. You want to minimize the rerenders</p>
       </blockquote>
       <section
-        className="Section--grid -centered"
+        className="Section--grid"
         css={css`
           color: black;
         `}
       >
         <div css={{ padding: "1rem" }}>
           <h1>0</h1>
-          <button>+ 1</button>
-          <button>- 1</button>
+          <button onClick={() => handleCount()}>+</button>
+          <button onClick={() => handleCount()}>-</button>
         </div>
       </section>
     </main>
